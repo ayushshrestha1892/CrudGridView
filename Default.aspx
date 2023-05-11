ba24442c5bd499ae823a3cbf8fdffb5f3fb4a35e
Ayush Shrestha <95868192+ayushshrestha1892@users.noreply.github.com>
2023-05-11 14:42:09 +0545

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,167 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CrudInGridView.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+            <script type="text/javascript">
+                $("[src*=plus]").live("click", function () {
+                    $(this).closest("tr").after("<tr><td></td><td colspan = '999'>" + $(this).next().html() + "</td></tr>")
+                    $(this).attr("src", "Images/minus.png");
+                });
+                $("[src*=minus]").live("click", function () {
+                    $(this).attr("src", "Images/plus.png");
+                    $(this).closest("tr").next().remove();
+                });
+            </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="gvPhoneBook" runat="server" AutoGenerateColumns="false" ShowFooter="true" DataKeyNames="PhoneBookID"
+                showHeaderWhenEmplty="true" AllowSorting="true"
+                OnRowCommand="gvPhoneBook_RowCommand" OnRowEditing="gvPhoneBook_RowEditing" OnRowCancelingEdit="gvPhoneBook_RowCancelingEdit" OnRowUpdating="gvPhoneBook_RowUpdating" OnRowDeleting="gvPhoneBook_RowDeleting" OnRowDataBound="gvPhoneBook_RowDataBound"
+                OnSorting="gvPhoneBook_Sorting"
+             
+                BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" OnSelectedIndexChanged="gcPhoneBook_SelectedIndexChanged">
+                <FooterStyle BackColor="White" ForeColor="#000066" />
+                <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                <RowStyle ForeColor="#000066" />
+                <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#00547E" />
+
+                <Columns>
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <img alt="" style="cursor: pointer" src="Images/plus.png"/>
+                            <asp:Panel ID="pnlDetail" runat="server" Style="display:none" >
+                                <asp:GridView ID="gvDetail" runat="server" AutoGenerateColumns="false">
+                                    <Columns>
+                                        <asp:BoundField ItemStyle-Width="150px" DataField="Address" HeaderText="Address"/>
+                                        <asp:BoundField ItemStyle-Width="150px" DataField="Gender" HeaderText="Gender"/>
+                                    </Columns>
+                                    <EmptyDataTemplate>
+                                        <div align="center">No records Found</div>
+                                    </EmptyDataTemplate>
+                                </asp:GridView>
+
+                            </asp:Panel>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="FirstName" SortExpression="FirstName">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("FirstName") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtFirstName" Text='<%# Eval("FirstName") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtFirstNameFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="LastName" SortExpression="LastName">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("LastName") %>' runat="server" />
+                        </ItemTemplate>
+
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtLastName" Text='<%# Eval("LastName") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtLastNameFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Contact" SortExpression="Contact">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Contact") %>' runat="server" />
+                        </ItemTemplate>
+
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtContact" Text='<%# Eval("Contact") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtContactFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Email" SortExpression="Email">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Email") %>' runat="server" />
+                        </ItemTemplate>
+
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtEmail" Text='<%# Eval("Email") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtEmailFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+
+                    
+                    <asp:TemplateField HeaderText="SortID">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("SortID") %>' runat="server" />
+                        </ItemTemplate>
+
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtSort" Text='<%# Eval("SortID") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtSortFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                     <asp:TemplateField HeaderText="Salary">
+                        <ItemTemplate>
+                            <asp:Label ID="salary" Text='<%# Eval("Salary") %>' runat="server" />
+                        </ItemTemplate>
+
+<%--                        <EditItemTemplate>
+                            <asp:TextBox ID="txtSalary" Text='<%# Eval("Salary") %>' runat="server" />
+                        </EditItemTemplate>--%>
+                        
+                        <FooterTemplate>
+                          <%--  <asp:TextBox ID="txtSalary" runat="server" />--%>
+                            <asp:Label ID="totalsal" runat="server">Total:</asp:Label>
+
+
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:ImageButton ImageUrl="~/Images/edit.png" runat="server" CommandName="Edit" ToolTip="Edit" Width="20px" Height="20px" />
+                            <asp:ImageButton ImageUrl="~/Images/delete.png" runat="server" CommandName="Delete" ToolTip="Delete" Width="20px" Height="20px" />
+
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:ImageButton ImageUrl="~/Images/save.png" runat="server" CommandName="Update" ToolTip="Update" Width="20px" Height="20px" />
+                            <asp:ImageButton ImageUrl="~/Images/cancel.png" runat="server" CommandName="Cancel" ToolTip="Cancel" Width="20px" Height="20px" />
+
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                       
+                            <asp:ImageButton ImageUrl="~/Images/addnew.png" runat="server" CommandName="AddNew" ToolTip="Add New" Width="20px" Height="20px" />
+                        </FooterTemplate>
+                    </asp:TemplateField>    
+                </Columns>
+            </asp:GridView>
+            <br />
+            <asp:Label ID="lblSuccessMessage" Text=" " runat="server" ForeColor="Green" />
+            <br />
+            <asp:Label ID="lblErrorMessage" Text=" " runat="server" ForeColor="Red" />
+        </div>
+    </form>
+
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace CrudInGridView
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        int total = 0;
+        string connectionString = @"Data Source=AYU;Integrated Security=true; Initial Catalog = PhoneBookDB";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) 
+            {
+                PopulateGridview();
+
+                var footlbl = gvPhoneBook.FooterRow.FindControl("totalsal") as Label;
+                if(footlbl != null)
+                {
+                    footlbl.Text = total.ToString();
+                }
+            }
+        }
+        protected void gcPhoneBook_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        void PopulateGridview()
+        {
+            DataTable dtbl = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM Phonebook", sqlCon);
+                sqlDa.Fill(dtbl);
+            }
+            if(dtbl.Rows.Count > 0)
+            {
+                gvPhoneBook.DataSource = dtbl;
+
+                ViewState["dt1"] = dtbl;
+                gvPhoneBook.DataBind();
+
+            }
+            else
+            { 
+                dtbl.Rows.Add(dtbl.NewRow());
+                gvPhoneBook.DataSource = dtbl;
+                gvPhoneBook.DataBind();
+                gvPhoneBook.Rows[0].Cells.Clear();
+                gvPhoneBook.Rows[0].Cells.Add(new TableCell());
+                gvPhoneBook.Rows[0].Cells[0].ColumnSpan = dtbl.Columns.Count;
+                gvPhoneBook.Rows[0].Cells[0].Text = "No Data Found.. ";
+                gvPhoneBook.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+            }
+
+        }
+
+        protected void gvPhoneBook_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName.Equals("AddNew"))
+                {
+                    using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                    {
+                        sqlCon.Open();
+                        string query = "INSERT INTO Phonebook(FirstName,LastName,Contact, Email) VALUES (@FirstName,@LastName,@Contact,@Email)";
+                        SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                        sqlCmd.Parameters.AddWithValue("@FirstName", (gvPhoneBook.FooterRow.FindControl("txtFirstNameFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@LastName", (gvPhoneBook.FooterRow.FindControl("txtLastNameFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Contact", (gvPhoneBook.FooterRow.FindControl("txtContactFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Email", (gvPhoneBook.FooterRow.FindControl("txtEmailFooter") as TextBox).Text.Trim());
+                        sqlCmd.ExecuteNonQuery();
+                        PopulateGridview();
+                        lblSuccessMessage.Text = "New Record Added";
+                        lblErrorMessage.Text = "";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+                throw;
+            }
+        }
+
+        protected void gvPhoneBook_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvPhoneBook.EditIndex = e.NewEditIndex;
+            PopulateGridview();
+        }
+
+        protected void gvPhoneBook_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvPhoneBook.EditIndex = -1;
+            PopulateGridview();
+        }
+
+        protected void gvPhoneBook_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                
+                    using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                    {
+                        sqlCon.Open();
+                        string query = "UPDATE Phonebook SET FirstName=@FirstName,LastName=@LastName,Contact=@Contact,Email=@Email WHERE PhoneBookID = @id";
+                        SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                        sqlCmd.Parameters.AddWithValue("@FirstName", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtFirstName") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@LastName", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtLastName") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Contact", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtContact") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Email", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtEmail") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvPhoneBook.DataKeys[e.RowIndex].Value.ToString()));
+                        sqlCmd.ExecuteNonQuery();
+                        gvPhoneBook.EditIndex = -1;
+
+
+                        PopulateGridview();
+                        lblSuccessMessage.Text = "Selected Record Updated";
+                        lblErrorMessage.Text = "";
+                    }
+       
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+        protected void gvPhoneBook_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "DELETE FROM Phonebook WHERE PhoneBookID = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvPhoneBook.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    PopulateGridview();
+                    lblSuccessMessage.Text = "Selected Record Deleted";
+                    lblErrorMessage.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+
+        }
+
+        private static DataTable GetData(string query)
+        {
+            string strConnString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(strConnString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = query;
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataSet ds = new DataSet())
+                        {
+                            DataTable dt = new DataTable();
+                            sda.Fill(dt);
+                            return dt;
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void gvPhoneBook_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if(e.Row.RowType == DataControlRowType.DataRow)
+            {
+                string phonebookId = gvPhoneBook.DataKeys[e.Row.RowIndex].Value.ToString();
+                GridView gvDetails = e.Row.FindControl("gvDetail") as GridView;
+                gvDetails.DataSource = GetData(string.Format("Select Address,Gender from Detail where phonebookid={0}",phonebookId));
+                gvDetails.DataBind();
+
+                var sallbl = e.Row.FindControl("salary") as Label;
+                if(sallbl != null)
+                {
+                    total += int.Parse(sallbl.Text);
+                }
+            }
+        }
+        public SortDirection dir
+        {
+            get
+            {
+                if (ViewState["diretionState"] == null)
+                {
+                    ViewState["directionState"] = SortDirection.Ascending;
+                }
+                return (SortDirection)ViewState["directionState"];
+            }
+            set { ViewState["directionState"] = value; }
+        }
+        protected void gvPhoneBook_Sorting(object sender, GridViewSortEventArgs e)
+        {
+
+            if (ViewState["dt1"] != null)
+            {
+                DataTable dt = (DataTable)ViewState["dt1"]; //store view state into the datatable
+                DataView dv = new DataView(dt); //convert datatable into dataview
+                string sortExpression = e.SortExpression ?? string.Empty; //initialize to empty string if
+
+                string sortDirection = ViewState["SortDirection"] as string;
+                if(sortDirection=="ASC" && sortExpression == ViewState["SortExpression"] as string)
+                {
+                    dv.Sort = sortExpression + " DESC"; //sort in desc order
+                    ViewState["SortDirection"] = "DESC";
+                }
+                else
+                {
+                    dv.Sort = sortExpression + " ASC"; //sort in ASC order
+                    ViewState["SortDirection"] = "ASC";
+
+                }
+                ViewState["SortExpression"] = sortExpression;
+                gvPhoneBook.DataSource = dv;
+                gvPhoneBook.DataBind();
+            }
+
+        }
+    }
+} 
